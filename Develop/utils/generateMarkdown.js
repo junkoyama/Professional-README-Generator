b59737da6ef9754ad59bfbff7051cf4470ecf619
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-01-02 21:11:42 -0500

    Added headers and template literals for index.js questions responses

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,36 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  Licensed under the ${data.license} license.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  Please feel free to contact me at ${data.questions} for any questions.
+
 `;
 }
 
